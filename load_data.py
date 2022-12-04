32577dca7b2381c3a2cd0f5493d7c72032457392
nemeczek <nemeczek@teamseasn.onmicrosoft.com>
2022-12-04 21:35:02 +0100

    clean data imports

--- load_data.py
+++ load_data.py
@@ -9,23 +9,7 @@ from targets import behavioral_traits
 # capture warnings
 warnings.simplefilter(action='ignore', category=RuntimeWarning)
 
-df_transactions_daily_prep = pd.read_csv(external_data_path + "/df_prep_accounts_detailed_daily_2021_12_01.csv")
-df_investorsonly_prep = pd.read_stata(project_path + "/data/04_prep_final_depots_by-token_onlyInvestors.dta")
-df_portfolios = pd.read_stata(project_path + "/data/03_prep_final_depots_by-token.dta")
-df_accounts_portfolios_full_l = pd.read_stata(project_path + "/data/02_prep_final_accounts_depots_full_l.dta")
-df_transactions_raw = pd.read_csv(project_path + "/data/raw/account_transactions_2022_09_06.csv")
-df_security_account_balances = pd.read_csv(project_path + "/data/raw/df_security_account_balances_prep_timeseries_2022-10-25.csv", low_memory=False)
-
-# prepare sub-category aggregated means (0 set to missing to avoid excess zeros)
-df_accounts_portfolios_full_l = df_accounts_portfolios_full_l.replace({'0': np.nan, 0: np.nan})
-df_sc_means = df_accounts_portfolios_full_l[['token', 'monthn'] +
-                                            list(df_accounts_portfolios_full_l.iloc[:, df_accounts_portfolios_full_l.columns.str.contains(r'^sc_*')].columns)] \
-    .groupby('token').agg(['mean']).reset_index()
-df_sc_means.columns = [col[0] for col in df_sc_means.columns.values]
-
-# merge df_sc_means to df_crypto_prep
-df_prep = df_portfolios.merge(df_sc_means,
-                              on='token')
+df_prep = pd.read_stata(project_path + "/data/03_prep_final_depots_by-token.dta")
 
 # set dummies
 df_prep['i_male_recoded'] = df_prep['i_male_imp'].map({'female': 0, 'male': 1})
